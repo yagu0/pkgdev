67a7cf8eb7d9e24cddd65aee710b7ccd2b1dda55
Benjamin Auder <benjamin.a@mailoo.org>
2015-02-01 12:02:53 +0100

    update

--- pkgTest/R/folder/b.R
+++ pkgTest/R/folder/b.R
@@ -1,6 +1,6 @@
 b = function() {
 	print("Hello, I'm b()")
 	r = 0
-	result = .C("d", result = as.integer(r), package="pkg_test")$result
+	result = .C("d", result = as.integer(r), package="pkgTest")$result
 	print(paste("The result is ", result))
 }
